c0868376d8be6cfae0d5eb3f15dc1d065cd01265
Tim Olsen <tolsen@limelabs.com>
2009-07-24 15:57:47 -0400

    pass LIMESTONE_HOST_DOMAIN to adduser, litmus, and Prestan Fixes #(4666) Refs #(4648)
    add Copyright 2009 to adduser.c

--- adduser/adduser.c
+++ adduser/adduser.c
@@ -1,4 +1,5 @@
 /* ====================================================================
+ * Copyright 2009 Lime Labs LLC
  * Copyright 2007 Lime Spot LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");

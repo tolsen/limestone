7737804568e2622232b6fa53c8c8058d9ff47486
Paritosh Shah <paritosh@limelabs.com>
2010-09-13 19:28:37 -0400

    fixed migrate_tags_to_properties, lb:tags values should now begin with the delimiter.

--- limestone/database/migrate_tags_to_properties.rb
+++ limestone/database/migrate_tags_to_properties.rb
@@ -14,6 +14,6 @@ db_connect do |dbh|
 
   bm_ns_id = get_namespace_id(dbh, bitmarks_ns)
   if bm_ns_id != -1
-    dbh.do("INSERT INTO properties (namespace_id, name, resource_id, xmlinfo, value) SELECT #{lb_ns_id}, 'tags', resources.id, '<P:tags xmlns:P=\"http://limebits.com/ns/1.0/\">', concat(bitmarks.value || ',') FROM resources INNER JOIN binds bitmarked_resources ON bitmarked_resources.name = resources.uuid INNER JOIN binds bitmark_resources ON bitmark_resources.collection_id = bitmarked_resources.resource_id INNER JOIN properties bitmarks ON bitmarks.resource_id = bitmark_resources.resource_id AND bitmarks.value != '' AND (bitmarks.namespace_id, bitmarks.name) IN ((#{bm_ns_id}, 'tag')) GROUP BY resources.id")
+    dbh.do("INSERT INTO properties (namespace_id, name, resource_id, xmlinfo, value) SELECT #{lb_ns_id}, 'tags', resources.id, '<P:tags xmlns:P=\"http://limebits.com/ns/1.0/\">', ',' || concat(bitmarks.value || ',') FROM resources INNER JOIN binds bitmarked_resources ON bitmarked_resources.name = resources.uuid INNER JOIN binds bitmark_resources ON bitmark_resources.collection_id = bitmarked_resources.resource_id INNER JOIN properties bitmarks ON bitmarks.resource_id = bitmark_resources.resource_id AND bitmarks.value != '' AND (bitmarks.namespace_id, bitmarks.name) IN ((#{bm_ns_id}, 'tag')) GROUP BY resources.id")
   end
 end

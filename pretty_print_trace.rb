14d121c14d21faf825cb5c7a5ce146b9b2679209
Paritosh Shah <paritosh@limelabs.com>
2010-10-19 13:50:52 -0400

    improved the pretty_print_trace script to work with dso's

--- pretty_print_trace.rb
+++ pretty_print_trace.rb
@@ -1,3 +1,12 @@
 ARGF.each_line do |l|
-    print l.gsub(/.*\[(0x.*)\]/) { |address| %x[addr2line -e aproot/bin/httpd -f #{$1}].gsub(/\n/, '') }
+    print l.gsub(/(.*)\[(0x.*)\]/) { |address| 
+        original_line = $1 + "[" + $2 + "]"
+        source_line = %x[addr2line -e aproot/bin/httpd -f #{$2}].gsub(/\n/, '');
+
+        if source_line.match(/\?/)
+            original_line
+        else
+            source_line
+        end
+    }
 end
